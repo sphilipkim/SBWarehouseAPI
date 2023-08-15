3ae3ece2300789ddd512448798a5e36f13f2cded
Philip Kim <sphilipkim@gmail.com>
2023-08-14 20:48:04 -0400

    230814 - create project
    Created Spring boot project

---
+++ src/main/java/com/sbwarehouse/sbwarehouse/SbwarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.sbwarehouse.sbwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbwarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbwarehouseApplication.class, args);
+	}
+
+}
